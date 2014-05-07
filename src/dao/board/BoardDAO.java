595dbe4665de694bb5b5ccfa091ba34839a05bdf
Vaert <shengh807@naver.com>
2014-05-07 23:51:57 +0900

    2차 Commit

--- src/dao/board/BoardDAO.java
+++ src/dao/board/BoardDAO.java
@@ -17,6 +17,12 @@ public class BoardDAO implements BoardDAOImpl{
 		this.sessionFactory = MyBatis.getSqlSessionFactory();
 	}
 
+	
+	/**
+	 * 게시판 목록 조회
+	 * @param boardModel
+	 * @return
+	 */
 	@Override
 	public List<BoardModel> selectList(BoardModel boardModel) {
 
@@ -33,6 +39,12 @@ public class BoardDAO implements BoardDAOImpl{
 		return null;
 	}
 
+	
+	/**
+	 * 게시판 수 조회
+	 * @param boardModel
+	 * @return
+	 */
 	@Override
 	public int selectCount(BoardModel boardModel) {
 		
@@ -44,5 +56,92 @@ public class BoardDAO implements BoardDAOImpl{
 		}
 		
 		return 0;
+	}
+
+	
+	/**
+	 * 게시판 상세 조회
+	 * @param boardModel
+	 * @return
+	 */
+	@Override
+	public BoardModel select(BoardModel boardModel) {
+		SqlSession session = this.sessionFactory.openSession();
+		try {
+			return session.selectOne("board.select", boardModel);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (session != null) session.close();
+		}
+		return null;
+	}
+
+	
+	/**
+	 * 게시판 등록 처리
+	 * @param boardModel
+	 */
+	@Override
+	public void insert(BoardModel boardModel) {
+		SqlSession session = this.sessionFactory.openSession();
+		try {
+			session.insert("board.insert", boardModel);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (session != null) session.close();
+		}
+	}
+
+	
+	/**
+	 * 게시판 수정 처리
+	 * @param boardModel
+	 */
+	@Override
+	public void update(BoardModel boardModel) {
+		SqlSession session = this.sessionFactory.openSession();
+		try {
+			session.update("board.update", boardModel);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (session != null) session.close();
+		}
+	}
+
+	
+	/**
+	 * 게시판 조회수 증가 수정 처리
+	 * @param boardModel
+	 */
+	@Override
+	public void updateHit(BoardModel boardModel) {
+		SqlSession session = this.sessionFactory.openSession();
+		try {
+			session.insert("board.updateHit", boardModel);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (session != null) session.close();
+		}
+	}
+
+	
+	/**
+	 * 게시판 삭제 처리
+	 * @param boardModel
+	 */
+	@Override
+	public void delete(BoardModel boardModel) {
+		SqlSession session = this.sessionFactory.openSession();
+		try {
+			session.delete("board.delete", boardModel);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (session != null) session.close();
+		}
 	}
 }
--- src/dao/board/BoardDAOImpl.java
+++ src/dao/board/BoardDAOImpl.java
@@ -19,4 +19,36 @@ public interface BoardDAOImpl {
 	 * @return
 	 */
 	public int selectCount(BoardModel boardModel);
+	
+	/**
+	 * 게시판 상세 조회
+	 * @param boardModel
+	 * @return
+	 */
+	public BoardModel select(BoardModel boardModel);
+	
+	/**
+	 * 게시판 등록 처리
+	 * @param boardModel
+	 */
+	public void insert(BoardModel boardModel);
+	
+	/**
+	 * 게시판 수정 처리
+	 * @param boardModel
+	 */
+	public void update(BoardModel boardModel);
+	
+	/**
+	 * 게시판 조회수 증가 수정 처리
+	 * @param boardModel
+	 */
+	public void updateHit(BoardModel boardModel);
+	
+	/**
+	 * 게시판 삭제 처리
+	 * @param boardModel
+	 */
+	public void delete(BoardModel boardModel);
+	
 }
